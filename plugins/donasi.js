33e9dcc0a7e9a47b388276c9b8cb30cb5a640c19
xmv504jr <firman31337@gmail.com>
2022-01-27 10:23:46 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,14 +1,6 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(reyganz + 'APIKEY')).buffer(), `
-*O W N E R*
-
-*SAWERIA:* https://saweria.co/inirey
-*TRAKKER:* https://trakteer.id/erzabot
-
-*P E N U L I S*
-
-*SAWERIA:* https://saweria.co/ariffb
-*TRAKKER:* https://trakteer.id/ariffb/tip
+*Gopay* = ###
 `.trim(), '© sekha', 'Donasi', '.donasi', m)
 handler.help = ['donasi']
 handler.tags = ['info']
